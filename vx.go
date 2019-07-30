4d0e15d2dc1f17a69b079097ec012579db2cab54
Ian Molee <imolee@gmail.com>
2019-07-30 00:28:28 -0700

    Accept "nah" and "yas" and add additional test cases

--- vx.go
+++ vx.go
@@ -3,16 +3,16 @@ package vx
 import "regexp"
 
 var (
-	regexProceed = regexp.MustCompile(`^\s*(kk?|yeh)\s*$`)
-	regexCancel  = regexp.MustCompile(`^\s*(naw(?:\s*dawg)?|w/e|ur\s+face|gtfo|no\s+u)\s*$`)
+	regexProceed = regexp.MustCompile(`(?i:^\s*(kk?|yeh|ya+s)\s*$)`)
+	regexCancel  = regexp.MustCompile(`(?i:^\s*(na[hw](?:\s*dawg)?|w/e|ur\s+face|gtfo|no\s+u)\s*$)`)
 )
 
-// Proceed indicates whether an operations should proceed given a response.
+// Proceed indicates whether an operation should proceed given a response.
 // The first return value indicates whether a response indicates whether to
 // proceed. The second return value is false if the response is not included
 // in the set of the acceptable responses, in which case the first return
 // value should be ignored, unless you roll like that.
-func Proceed(resp string) (bool, bool) {
+func Proceed(resp string) (proceed, valid bool) {
 	if regexProceed.MatchString(resp) {
 		return true, true
 	}
--- vx_test.go
+++ vx_test.go
@@ -12,30 +12,44 @@ func TestProceed(t *testing.T) {
 		proceed, valid bool
 	}{
 		{"k", true, true},
+		{"K", true, true},
 		{"kk", true, true},
+		{"kK", true, true},
 		{" k ", true, true},
 		{"kkk", false, false},
+		{"yas", true, true},
+		{"yaas", true, true},
+		{"YAAAAAAS", true, true},
 		{"yeh", true, true},
 		{" yeh ", true, true},
+		{" YEH\t", true, true},
 		{"yeah", false, false},
 		{"w/e", false, true},
+		{"W/E", false, true},
 		{"\tw/e ", false, true},
 		{"naw", false, true},
+		{"NAW DAWG", false, true},
 		{"naw dawg", false, true},
 		{"naw\tdawg", false, true},
 		{"naw   dawg", false, true},
+		{"nah", false, true},
+		{"nah dawg", false, true},
 		{"ur face", false, true},
+		{"Ur Face", false, true},
 		{"ur  face", false, true},
 		{"gtfo", false, true},
+		{"GTFO", false, true},
+		{"NO U", false, true},
+		{"no u", false, true},
 	}
 	for _, tc := range tt {
 		t.Run(tc.in, func(t *testing.T) {
 			gotProceed, gotValid := vx.Proceed(tc.in)
+			if want, got := tc.valid, gotValid; want != got {
+				t.Fatalf("%q: wanted valid value = %t, got %t", tc.in, want, got)
+			}
 			if want, got := tc.proceed, gotProceed; want != got {
 				t.Errorf("%q: wanted proceed value = %t, got %t", tc.in, want, got)
-			}
-			if want, got := tc.valid, gotValid; want != got {
-				t.Errorf("%q: wanted valid value = %t, got %t", tc.in, want, got)
 			}
 		})
 	}
